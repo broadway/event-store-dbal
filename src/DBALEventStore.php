80f194eb86f51b3473e4d8d58abda0c5d33b2fdb
Christian Hammerl <info@christian-hammerl.de>
2017-11-05 23:52:26 +0100

    fix interface

--- src/DBALEventStore.php
+++ src/DBALEventStore.php
@@ -81,7 +81,7 @@ class DBALEventStore implements EventStore, EventStoreManagement
     /**
      * {@inheritDoc}
      */
-    public function load($id)
+    public function load($id): DomainEventStream
     {
         $statement = $this->prepareLoadStatement();
         $statement->bindValue(1, $this->convertIdentifierToStorageValue($id));
@@ -103,7 +103,7 @@ class DBALEventStore implements EventStore, EventStoreManagement
     /**
      * {@inheritDoc}
      */
-    public function loadFromPlayhead($id, $playhead)
+    public function loadFromPlayhead($id, int $playhead): DomainEventStream
     {
         $statement = $this->prepareLoadStatement();
         $statement->bindValue(1, $this->convertIdentifierToStorageValue($id));
