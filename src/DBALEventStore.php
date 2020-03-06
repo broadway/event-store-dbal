94528a9bac27a11ee6d6f7b0305fe9ba0f0ae6b8
othillo <othillo@othillo.nl>
2020-03-06 18:20:40 +0100

    added license

--- src/DBALEventStore.php
+++ src/DBALEventStore.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the broadway/broadway package.
+ * This file is part of the broadway/event-store-dbal package.
  *
- * (c) Qandidate.com <opensource@qandidate.com>
+ * (c) 2020 Broadway project
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
